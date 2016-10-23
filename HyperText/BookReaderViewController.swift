5c6d54107ad4e4ef466826b0eaae5b39db7b929f
Katherine <kbruton@utexas.edu>
2016-10-23 14:38:32 -0500

    Back button/nav controller for book reader view

--- HyperText/BookReaderViewController.swift
+++ HyperText/BookReaderViewController.swift
@@ -10,17 +10,14 @@ import UIKit
 
 class BookReaderViewController: UIViewController {
 
-    @IBOutlet weak var bookTitle: UILabel!
     var book:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title="\(book)";
 
         // Do any additional setup after loading the view.
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        bookTitle.text = book
     }
 
     override func didReceiveMemoryWarning() {
